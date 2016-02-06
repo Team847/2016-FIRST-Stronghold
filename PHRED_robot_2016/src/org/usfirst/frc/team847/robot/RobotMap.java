692ec175d7d003c75111057b1ce08d34d6ca327b
team847 <team847@k3pt.com>
2016-02-06 15:09:46 -0800

    git test commit

--- PHRED_robot_2016/src/org/usfirst/frc/team847/robot/RobotMap.java
+++ PHRED_robot_2016/src/org/usfirst/frc/team847/robot/RobotMap.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team847.robot;
 public interface RobotMap {
 
 //==========================DRIVE TRAIN======================//
-	
+	int temp = 0; //git test should be removed
 	
 	
 	
