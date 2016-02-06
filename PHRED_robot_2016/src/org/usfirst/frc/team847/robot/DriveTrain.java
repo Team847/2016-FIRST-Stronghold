77f832d95c4e2c253d87ed041573267df01c64dd
Team 847 <team847@k3pt.com>
2016-02-06 14:12:54 -0800

    Changes to Drivetain, obstaclearm, and robotmap

--- PHRED_robot_2016/src/org/usfirst/frc/team847/robot/DriveTrain.java
+++ PHRED_robot_2016/src/org/usfirst/frc/team847/robot/DriveTrain.java
@@ -1,5 +1,119 @@
 package org.usfirst.frc.team847.robot;
 
-public class DriveTrain {
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Encoder;
 
+public class DriveTrain {
+	
+CANTalon backMotor;
+CANTalon leftMotor;
+CANTalon rightMotor;
+CANTalon turnMotor;
+Encoder turnCounter;
+
+
+double speed1;
+double speed2;
+double speed3;
+
+double frameLength = 23;
+double frameWidth = 23;
+double speedLeft;
+double speedRight;
+double speedBack;
+double radianradianDirection;
+double longRadius;
+double middleRadius;
+double shortRadius;
+int ninetyClicks;
+
+public void TriDrive() {
+	
+	backMotor = new CANTalon(1);
+	leftMotor = new CANTalon(2);
+	rightMotor = new CANTalon(4);
+	turnMotor = new CANTalon(3);
+	
+	backMotor.set(0);
+	leftMotor.set(0);
+	rightMotor.set(0);
+	turnMotor.set(0);
+
+}	
+
+public void turnWheel(double radianDirection){
+	
+	int turn = (int)radianDirection*ninetyClicks;
+	int position = turnMotor.getEncPosition();
+	
+	if(position>ninetyClicks){
+		turnMotor.set(0);
+	}
+	if(position<-ninetyClicks){
+		turnMotor.set(0);
+	}
+	if(position>turn){
+		turnMotor.set(-.25);
+	}
+	if(position<turn){
+		turnMotor.set(.25);
+	}
+	if(position==turn){
+		turnMotor.set(0);
+	}
+	
+
+}
+public void driveWheels(double speed, double radianDirection){
+
+	
+radianradianDirection = turnMotor.getEncPosition()*Math.PI/(ninetyClicks*2);
+//this checks to make it go straight
+if (radianradianDirection > -0.01 && radianDirection <0.1){
+speedLeft = speed;
+speedRight = speed;
+speedBack = speed;
+}
+if (radianDirection >= 0.01 && radianDirection <= Math.PI/2 ) {
+middleRadius = frameLength/(Math.sin(radianradianDirection));
+shortRadius = (middleRadius*Math.cos(radianradianDirection))-(frameWidth/2);
+longRadius = (shortRadius+frameWidth);
+speedRight = speed;
+speedLeft = speed * shortRadius/middleRadius;
+//Rg I'm A Pirate (An Orca PIRATE)
+speedBack = speed * middleRadius/longRadius;
+if (speedBack>speedRight){
+speed1 = speedBack;
+speedBack = speedRight;
+speedRight = speed1;
+
+}
+}
+backMotor.set(speedBack);
+leftMotor.set(speedLeft);
+rightMotor.set(speedRight);
+
+
+
+
+
+if (radianDirection <= -0.01 && radianDirection >= Math.PI/-2) {
+middleRadius = frameLength/(Math.sin(radianradianDirection));
+shortRadius = (middleRadius*Math.cos(radianradianDirection))-(frameWidth/2);
+longRadius = (shortRadius+frameWidth);
+speedRight = speed;
+speedLeft = speed * shortRadius/middleRadius;
+//Rg I'm A Pirate (An Orca PIRATE)
+speedBack = speed * middleRadius/longRadius;
+if (speedBack>speedRight){
+speed1 = speedBack;
+speedBack = speedLeft;
+speedLeft = speed1;
+
+}
+}
+backMotor.set(speedBack);
+leftMotor.set(speedRight);
+rightMotor.set(speedLeft);
+}
 }
--- PHRED_robot_2016/src/org/usfirst/frc/team847/robot/ObstacleArm.java
+++ PHRED_robot_2016/src/org/usfirst/frc/team847/robot/ObstacleArm.java
@@ -1,5 +1,98 @@
 package org.usfirst.frc.team847.robot;
 
-public class ObstacleArm {
+import edu.wpi.first.wpilibj.*;
 
+public class ObstacleArm {
+	
+	CANTalon Elbow = new CANTalon(1);
+	CANTalon Shoulder = new CANTalon(2);
+	double pReader = Shoulder.getPosition();
+	boolean shoulderFWD = false;
+	boolean shoulderREV = false;
+	boolean elbowFWD = false;
+	boolean elbowREV = false;
+	boolean buttonMash = false;
+	
+	public ObstacleArm(){
+	}
+
+	public void shoulderJoint(boolean reachUP, boolean reachDOWN){
+		/*  Robot Frame should send me boolean reachUP so this code
+		    can decide how the arm should move. */
+		shoulderFWD = Shoulder.isFwdLimitSwitchClosed();
+		shoulderREV = Shoulder.isRevLimitSwitchClosed();
+		
+		/*stops the motors from doing anything if BOTH buttons are pressed
+		 * cuz who knows what would happen if both were pressed at the same
+		 * time...?? I have no clue. don't be a buttonmasher!*/
+		if(reachUP == true && reachDOWN == true){
+			buttonMash = true;
+		}
+		else{
+			buttonMash = false;
+		}
+		
+		/* pretty much everything in this class below this point is receiving
+		 * the state of reachUP and reachDOWN and telling the motors what to do
+		 * from there.*/
+		if(buttonMash = true){
+			Shoulder.set(0);
+		}
+		else{
+			if(reachUP == true){
+				if(shoulderFWD == true){
+					Shoulder.set(0);
+			}
+				else{ 
+					if(shoulderFWD == false){
+						Shoulder.set(-.2);
+				}
+			}
+		}
+		
+		if(reachDOWN == true){
+			if(shoulderREV == true){
+				Shoulder.set(0);
+			}
+			else{
+				if(shoulderREV == false){
+					Shoulder.set(.2);
+				}
+			}
+		}
+	}
+}
+	
+	/*   (UNUSED CLASS)VVVVVVVVV  
+	 *It turns out we aren't using this elbow joint but it's still here cuz 
+	 *it doesn't hurt anything by staying, and it would be a pain to take it 
+	 *all out and then realize a day later that I actually wanted it...*/
+	public void elbowJoint(boolean reachOUT, boolean reachIN){
+	//obsolete now :P
+		elbowFWD = Elbow.isFwdLimitSwitchClosed();
+		elbowREV = Elbow.isRevLimitSwitchClosed();
+		
+		if(reachOUT = true){
+			if(elbowFWD = true){
+				Elbow.set(0);
+			}
+			else{ 
+				if(elbowFWD = false){
+					Elbow.set(-.2);
+				}
+			}
+		}
+		
+		if(reachIN = true){
+			if(elbowREV = true){
+				Elbow.set(0);
+			}
+			else{
+				if(elbowREV = false){
+					Elbow.set(.2);
+				}
+			}
+		}
+
+	}
 }
