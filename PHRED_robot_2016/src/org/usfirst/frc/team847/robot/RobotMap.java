5ece7bcec14b67a810eae93c003a2c84363c0c23
team847 <team847@k3pt.com>
2016-02-09 19:08:31 -0800

    added varables to robotmap ryan

--- PHRED_robot_2016/src/org/usfirst/frc/team847/robot/RobotMap.java
+++ PHRED_robot_2016/src/org/usfirst/frc/team847/robot/RobotMap.java
@@ -4,11 +4,11 @@ public interface RobotMap {
 
 //==========================DRIVE TRAIN======================//	
 	
+	public final static double FRAME_LENGTH = 23;
+	public final static double FRAME_WIDTH  = 23;
 	
 	
-	
-	
-	
+	 
 	//==================sensors=============================\\
 	
 	
@@ -25,8 +25,12 @@ public interface RobotMap {
 	
 	
 	
+	//=================================ARM=================================//
 	
-	
-	
-	
+	public final static boolean ELBOW_REV = false;
+	public final static boolean ELBOW_FWD = false;
+	public final static boolean SHOULDER_FWD = false;
+	public final static boolean SHOULDER_REV = false;
+	public final static boolean BUTTON_MASH = false;
+	public final static double P_READER = 0; 
 }
