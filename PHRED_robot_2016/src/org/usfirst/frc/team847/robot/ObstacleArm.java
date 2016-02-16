6f30a3127aa0ab9457bda4875afe3ea394c1fa1f
Team 847 <team847@k3pt.com>
2016-02-15 19:46:45 -0800

    Added the new BallShooter

--- PHRED_robot_2016/src/org/usfirst/frc/team847/robot/ObstacleArm.java
+++ PHRED_robot_2016/src/org/usfirst/frc/team847/robot/ObstacleArm.java
@@ -3,78 +3,110 @@ package org.usfirst.frc.team847.robot;
 import edu.wpi.first.wpilibj.*;
 
 public class ObstacleArm {
-
+	
 	CANTalon Elbow = new CANTalon(1);
-	CANTalon Shoulder = new CANTalon(1);
-	double pShoulder;
-	double pElbow;
-	boolean shoulderFWD;
-	boolean shoulderREV;
-	boolean elbowFWD;
-	boolean elbowREV;
-	boolean shoulderMash;
-	boolean elbowMash;
+	CANTalon Shoulder = new CANTalon(2);
+	double pReader = 0;
+	boolean shoulderFWD = false;
+	boolean shoulderREV = false;
+	boolean elbowFWD = false;
+	boolean elbowREV = false;
+	boolean buttonMash = false;
 	/*Note to Stephen: When you pull all the pieces together for the robot map
 	 *You'll have to create two xbox controller classes, one being the drive 
 	 *controller and the other being the object manip controller. This class here
 	 *needs to get all it's values from the object manip controller.*/
+	
 	public ObstacleArm(){
-		Shoulder.set(0);
-		Elbow.set(0);
+	}
+
+	public void shoulderJoint(boolean reachUP, boolean reachDOWN){
+		
+		/*  Robot Frame should send me boolean reachUP so this code
+		    can decide how the arm should move. */
 		shoulderFWD = Shoulder.isFwdLimitSwitchClosed();
 		shoulderREV = Shoulder.isRevLimitSwitchClosed();
-		Shoulder.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogPot);
-		Elbow.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogPot);
-		pShoulder = Shoulder.getAnalogInPosition();
-		pElbow = Elbow.getAnalogInPosition();
-	}
-	public void shoulderJoint(boolean reachUP, boolean reachDOWN, double minS, double maxS){
-
-		/*  The Robot Framework program should send this method a handful of 
-		 *  button values in the form of booleans to decide which way to move
-		 * The code below will stop the motors from doing anything if BOTH 
-		 * buttons are pressed, cuz who knows what would happen if both were pressed
-		 * at the same time...?? I have no clue. don't be a buttonmasher!*/
+		pReader = Shoulder.getPosition();
 		
+		/*stops the motors from doing anything if BOTH buttons are pressed
+		 * cuz who knows what would happen if both were pressed at the same
+		 * time...?? I have no clue. don't be a buttonmasher!*/
 		if(reachUP == true && reachDOWN == true){
-			shoulderMash = true;
+			buttonMash = true;
 		}
 		else{
-			shoulderMash = false;
+			buttonMash = false;
 		}
-		if(shoulderMash = true){
+		
+		/* pretty much everything in this class below this point is receiving
+		 * the state of reachUP and reachDOWN and telling the motors what to do
+		 * from there.*/
+		if(buttonMash = true){
 			Shoulder.set(0);
 		}
 		else{
-			if(pShoulder == maxS || shoulderFWD == true){
+			if(reachUP == true){
+				if(shoulderFWD == true){
+					Shoulder.set(0);
+			}
+				else{ 
+					if(shoulderFWD == false){
+						Shoulder.set(-.2);
+				}
+			}
+		}
+		
+		if(reachDOWN == true){
+			if(shoulderREV == true){
 				Shoulder.set(0);
 			}
-			else if(reachUP == true){
-				Shoulder.set(-.2);
-				}
-			
-		if(pShoulder == minS || shoulderREV == true){
-			Shoulder.set(0);
-			}
-			else if(reachDOWN == true){
-				Shoulder.set(.2);
+			else{
+				if(shoulderREV == false){
+					Shoulder.set(.2);
 				}
 			}
 		}
-	public void elbowJoint(boolean reachUP, boolean reachDOWN, double minE, double maxE){
+	}
+		return;
+}
+	
+	public void presets(boolean upPos, boolean drivePos, double upPosition, double drivePosition){
 		
+		if(upPos == true && pReader > drivePosition && pReader < upPosition){
+			Shoulder.set(.2);
+		}
+		else{
+			if(drivePos == true && pReader > drivePosition && pReader < upPosition){
+				Shoulder.set(-.2);
+			}
+			else{
+				Shoulder.set(0);
+			}
+		}
+	}
+	
+	/*   (UNUSED CLASS)VVVVVVVVV  
+	 *It turns out we aren't using this elbow joint but it's still here cuz 
+	 *it doesn't hurt anything by staying, and it would be a pain to take it 
+	 *all out and then realize a day later that I actually wanted it...*/
+	public void elbowJoint(boolean reachOUT, boolean reachIN){
+	//obsolete now :P
 		elbowFWD = Elbow.isFwdLimitSwitchClosed();
 		elbowREV = Elbow.isRevLimitSwitchClosed();
 		
-		if(elbowFWD == true || pElbow == maxE){
+		if(reachOUT = true){
+			if(elbowFWD = true){
 				Elbow.set(0);
 			}
-			else{
-				if(reachDOWN == true){
-				Elbow.set(-.2);
+			else{ 
+				if(elbowFWD = false){
+					Elbow.set(-.2);
 				}
 			}
-		if((pElbow == minE || elbowREV == true) && reachUP == true){
+		}
+		
+		if(reachIN = true){
+			if(elbowREV = true){
 				Elbow.set(0);
 			}
 			else{
@@ -84,67 +116,5 @@ public class ObstacleArm {
 			}
 		}
 
-	/* Hey Stephen!! This class wants six variables to work,
-		so we gotta make sure it has them all.  */
-	
-	public void presets(boolean upPosButton, boolean drivePosButton, double SupPosition, double SdrivePosition, double EupPosition, double EdrivePosition){
-		
-		if(upPosButton == true && pShoulder < SupPosition){
-			Shoulder.set(.2);
-		}
-		else{
-			if(drivePosButton == true && pShoulder > SdrivePosition){
-				Shoulder.set(-.2);
-			}
-			else{
-				Shoulder.set(0);
-			}
-		}
-		
-		if(upPosButton == true && pElbow < EupPosition){
-			Elbow.set(.2);
-		}
-		else{
-			if(drivePosButton == true && pElbow > EdrivePosition){
-				Elbow.set(-.2);
-			}
-			else{
-				Elbow.set(0);
-			}
-		}
 	}
 }
-
-
-
-
-
-
-
-/*  
- *  
- *  __-  
- * |\  _~`|                                                   
- * | \  -~`|                                                              
- * |  \  _~`|                                                            
- * |   \  -~`|                                                              
- *  \   \  -~`|                                                               
- *   \   \  _~`|                                                             
- *    \   \  _~`|                                                              
- *     \   \  -~`|                                                           
- *      \   \  -~`|`                                                               
- *       \   \  -~`|                                                              
- *        \   \  _~`|                                                               
- *         \   \  -~`|                                                                 
- *          \   \  _~`|                                                            
- *           \   \  _~`|                                                              
- *            \   \  -~`|                                                                
- *             \   \  _~``|  |  |  |  |  |  |  |  |  |  |  |  |  |  |  ||                                                         
- *              \   \  -~ ~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`~`\                                                     
- *               \   \  _ -- _ - - _ _ - _ __ -- _ - - _ _ - _ __ -- _ - - \
- *                \   \_____________________________________________________\
- *                 \  |                                                     |
- *                  \ |                                                     |
- *                   \|_____________________________________________________|
- */
-
