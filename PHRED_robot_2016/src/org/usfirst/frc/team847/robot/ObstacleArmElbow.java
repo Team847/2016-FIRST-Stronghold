2b6c6d49f8a91dce33d96f7771ea46b726550097
Daniel <daniscool509@gmail.com>
2016-02-16 20:22:18 -0800

    Adding a new class for the other arm - Z

---
+++ PHRED_robot_2016/src/org/usfirst/frc/team847/robot/ObstacleArmElbow.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team847.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class ObstacleArmElbow{
+	
+	public ObstacleArmElbow(){
+//		Elbow.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogPot);
+//		pElbow = Elbow.getAnalogInPosition();
+	}
+	
+	public void elbowJoint(boolean reachUP, boolean reachDOWN, double minE, double maxE){
+	}
+}
