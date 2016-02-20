9a72c9eb945bae19cb67dea3490632c0f5757418
Zane <zweickum@gmail.com>
2016-02-19 20:19:17 -0800

    Committing robot map, robot.java, ballshooter and obstacle arm

--- PHRED_robot_2016/src/org/usfirst/frc/team847/robot/RobotMap.java
+++ PHRED_robot_2016/src/org/usfirst/frc/team847/robot/RobotMap.java
@@ -1,39 +1,33 @@
 package org.usfirst.frc.team847.robot;
 public interface RobotMap {
 
-//==========================DRIVE TRAIN======================//	
+	//==========================DRIVE TRAIN======================//	
 	
 	public final static double FRAME_LENGTH = 23;
 	public final static double FRAME_WIDTH  = 23;
 	
 	
 	 
-	//==================sensors=============================\\
+	//==================Robot.Java=============================\\
 	
-
-	
-	
-	
-	
-	
+	public final static int DRIVE_GAMEPAD = 1;
+	public final static int OBJ_MANIP_GAMEPAD = 2;
 	
 	//============================Ball shooter===========//
 	 
-	 public final static int FLAG = 0;
+	 public final static double BOTTOM_SHOOTER_MOTOR_SHOOTING_ROUTINE = 1.0;
+	 public final static double TOP_SHOOTER_MOTOR_SHOOTING_ROUTINE = -1.0;
 	 public final static int SHOOT = 1;
 	 public final static int INTAKE = 3;
-	 public final static int EXPELL = 2 ;
-	 public final static int I = 0;
-	    
-
-	    
+	 public final static int EXPELL = 2;	    
 	    
 	//=================================ARM=================================//
 	
-	public final static boolean ELBOW_REV = false;
-	public final static boolean ELBOW_FWD = false;
-	public final static boolean SHOULDER_FWD = false;
-	public final static boolean SHOULDER_REV = false;
-	public final static boolean BUTTON_MASH = false;
+	public final static int CANTALON_ELBOW = 1;
+	public final static int CANTALON_SHOULDER =2;
+	public final static double MIN_S = 0;
+	public final static double MAX_S = 5;
+	public final static double MIN_E = 0;
+	public final static double MAX_E = 5;
 	public final static double P_READER = 0; 
 }
