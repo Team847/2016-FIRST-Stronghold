4cdc545b2d0feb8999a1ac6e522ef02c25f2b96e
daniscool509 <daniscool509@gmail.com>
2016-03-05 13:47:22 -0800

    Autonomus Programs

---
+++ PHRED_robot_2016/src/org/usfirst/frc/team847/robot/GRIP.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team847.robot;
+
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class GRIP {
+NetworkTable table; 
+
+
+public void reflectiveTape(){
+	
+}
+}
